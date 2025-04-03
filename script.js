7ec4603a74883b7532107141e06a45cf5aed850e
Harsh-Codes-77 <pharshpathak703@gmail.com>
2025-04-03 11:27:57 +0530

    Solve Cursor Problem

--- script.js
+++ script.js
@@ -94,59 +94,64 @@ function locomotiveAnimation() {
   }
   function cursorAnimation() {
     Shery.mouseFollower({
-      skew: true,
-      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-      duration: 1,
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
     });
     Shery.makeMagnet("#nav-part2 h4");
-  
+
     var videoContainer = document.querySelector("#video-container");
-    var video = document.querySelector("#video-container video")
+    var video = document.querySelector("#video-container video");
     videoContainer.addEventListener("mouseenter", function () {
-      videoContainer.addEventListener("mousemove", function (dets) {
         gsap.to(".mousefollower", {
-          opacity: 0
+            opacity: 0,
+            duration: 0.3
         });
         gsap.to("#video-cursor", {
-          left: dets.x - 570,
-          y: dets.y - 300,
+            opacity: 1, 
+            duration: 0.3
         });
-      });
+    });
+    videoContainer.addEventListener("mousemove", function (dets) {
+        gsap.to("#video-cursor", {
+            left: dets.x - 570,
+            y: dets.y - 300,
+        });
     });
     videoContainer.addEventListener("mouseleave", function () {
-      gsap.to(".mousefollower", {
-        opacity: 1
-  
-      });
-      gsap.to("#video-cursor", {
-        left: "70%",
-        top: "-15%",
-      });
+        gsap.to(".mousefollower", {
+            opacity: 1,
+            duration: 0.3
+        });
+        gsap.to("#video-cursor", {
+            left: "70%",
+            top: "-15%",
+            opacity: 1, 
+            duration: 0.3
+        });
     });
-  
-  
-  
-    var flag = 0
+
+    var flag = 0;
     videoContainer.addEventListener("click", function () {
-      if (flag == 0) {
-        video.play()
-        video.style.opacity = 1
-        document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`
-        gsap.to("#video-cursor", {
-          scale: 0.5
-        })
-        flag = 1
-      } else {
-        video.pause()
-        video.style.opacity = 0
-        document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`
-        gsap.to("#video-cursor", {
-          scale: 1
-        })
-        flag = 0
-      }
-    })
-  }
+        if (flag == 0) {
+            video.play();
+            video.style.opacity = 1;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`;
+            gsap.to("#video-cursor", {
+                scale: 0.5
+            });
+            flag = 1;
+        } else {
+            video.pause();
+            video.style.opacity = 0;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`;
+            gsap.to("#video-cursor", {
+                scale: 1
+            });
+            flag = 0;
+        }
+    });
+}
   function sheryAnimation() {
     Shery.imageEffect(".image-div", {
       style: 5,
